d55b1c75980511a44c21e8bcc30ea24850ec85c3
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2020-01-09 17:39:01 +0100

    Updates

---
+++ precache-manifest.dbfc151208d2bf4f17e9d0c552b4a8e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b9e8ae718e9b4318adbb9ab4a77400",
+    "url": "/nianFusk/index.html"
+  },
+  {
+    "revision": "c9dc5dae1525949ee26b",
+    "url": "/nianFusk/static/css/main.1d18bd30.chunk.css"
+  },
+  {
+    "revision": "c5ffe19c7d1b7e03c656",
+    "url": "/nianFusk/static/js/2.992d712a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nianFusk/static/js/2.992d712a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9dc5dae1525949ee26b",
+    "url": "/nianFusk/static/js/main.8a6d691e.chunk.js"
+  },
+  {
+    "revision": "f1f2b5076978744c6d48",
+    "url": "/nianFusk/static/js/runtime-main.8fcc49be.js"
+  }
+]);
